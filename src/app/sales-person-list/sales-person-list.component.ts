42def57e889814ff5160a72b0bbdfbdf1a74f715
ozark-dev <jason@ozarkdev.com>
2023-04-26 23:09:07 -0500

    Added salesPersonList array of SalesPerson instances

--- src/app/sales-person-list/sales-person-list.component.ts
+++ src/app/sales-person-list/sales-person-list.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SalesPerson } from './sales-person';
 
 @Component({
   selector: 'app-sales-person-list',
   templateUrl: './sales-person-list.component.html',
-  styleUrls: ['./sales-person-list.component.css']
+  styleUrls: ['./sales-person-list.component.css'],
 })
-export class SalesPersonListComponent {
+export class SalesPersonListComponent implements OnInit {
+  // create an array of objects
+  salesPersonList: SalesPerson[] = [
+    new SalesPerson('Anup', 'Kumar', 'anup@kaminocode.com', 50000),
+    new SalesPerson('John', 'Doe', 'john@kaminocode.com', 40000),
+    new SalesPerson('Claire', 'Murphy', 'claire@kaminocode.com', 90000),
+    new SalesPerson('Mai', 'Truong', 'mai@kaminocode.com', 60000),
+  ];
 
+  constructor() {}
+
+  ngOnInit() {}
 }
